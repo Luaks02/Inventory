5be49b927857bde0e7462f1a0a1aeeea3d3994e7
Lucas Martins <Lucascamilo02@gmail.com>
2022-11-22 16:03:59 -0300

    Study about openpyxl and some examples, will be added more openpyxl examples soon...

--- excel_test.py
+++ excel_test.py
@@ -5,6 +5,13 @@ wb = load_workbook('Estoque.xlsx')
 ws = wb.active
 
 for row in range(1,11):
-    ws['A' + str(row)] = row
+    for col in range(1,6):
+        char = get_column_letter(col)
+        ws[char + str(row)] = char + str(row)
+
+ws.insert_rows(4)
+
+ws.delete_rows(4)
+
 
 wb.save('Estoque.xlsx')
