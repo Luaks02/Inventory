85302d154a0877c3b5fbec8f2ee1a1140b2a4068
Lucas Martins <Lucascamilo02@gmail.com>
2022-11-23 13:59:59 -0300

    implemented new function to add new items asking for input for each attribute

--- Inventory.py
+++ Inventory.py
@@ -1,6 +1,13 @@
 from openpyxl import workbook, load_workbook
 from openpyxl.utils import get_column_letter
 
+#Abrindo a planilha e iniciando variavel de quantidade de linhas já usadas no Estoque
+wb = load_workbook('Estoque.xlsx')
+ws = wb["SmartHaus"]
+
+end_row = len(ws["A"])
+
+#Iniciando classe de objeto para organizar as informações de cada produto
 class Produtos:
     def __init__(self,nome,qnt,valor,descricao,marca):
         self.nome = nome
@@ -9,25 +16,64 @@ class Produtos:
         self.descricao = descricao
         self.marca = marca
 
+#Subindo as informações da planilha banco de dados para o código
 estoque = []
-estoque.append(Produtos("micro_inversor_151506_4",0,2998,"SUN2000G3-US-220 2KW MONOFASICO 220V 4MPPT MONITORAMENTO","DEYE"))
-estoque.append(Produtos("inversor_hibrido_177342_2",0,8639,"SUN-3K-SG04LP1-EU 3KW MONOFASICO 220V MPPT MONITORAMENTO","DEYE"))
-estoque.append(Produtos("inversor_hibrido_177343_6",0,10879,"SUN-5K-SG04LP1-EU 5KW MONOFASICO 220V 2MPPT MONITORAMENTO","DEYE"))
-estoque.append(Produtos("inversor_hibrido_177346_8",0,19719,"SUN-8K-SG04LP1-EU 8KW MONOFASICO 127/220V 2MPPT MONITORAMENTO","DEYE"))
-estoque.append(Produtos("inversor_hibrido_177345_4",0,24139,"SUN-10K-SG04LP1-EU 10KW TRIFASICO 380V 2MPPT MONITORAMENTO","DEYE"))
-estoque.append(Produtos("inversor_on_grid_68207_0",0,2229,"MIC 1500 TL-X 1.5KW MONOFASICO 220V 1MPPT MONITORAMENTO","GROWATT"))
-estoque.append(Produtos("inversor_on_grid_68208_4",0,2449,"MIC 2000 TL-X 2KW MONOFASICO 220V 1MPPT MONITORAMENTO","GROWATT"))
-estoque.append(Produtos("inversor_on_grid_68210_5",0,2539,"MIC 2500 TL-X 2.5KW MONOFASICO 220V 1MPPT MONITORAMENTO","GROWATT"))
-estoque.append(Produtos("inversor_on_grid_68211_9",0,2719,"MIC 3000 TL-X 3KW MONOFASICO 220V 1MPPT MONITORAMENTO","GROWATT"))
-estoque.append(Produtos("inversor_on_grid_68202_0",0,3509,"MIC 3000 TL-X 3KW MONOFASICO 220V 2MPPT MONITORAMENTO","GROWATT"))
-
-wb = load_workbook('Estoque.xlsx')
-ws = wb["SmartHaus"]
-
-end_row = len(ws["A"])
 
 for row in range(2,end_row+1):
-    for produto in  estoque:
-        if produto.nome == ws["A"+str(row)].value:
-            produto.qnt = ws["B"+str(row)].value
-            print("O produto {} ainda tem {} unidades".format(produto.nome,produto.qnt))
+    estoque.append(Produtos(ws["A"+str(row)].value,ws["B"+str(row)].value,ws["C"+str(row)].value,ws["D"+str(row)].value,ws["E"+str(row)].value,))
+
+#Adicionando novos produtos ao banco de dados
+def adicionar_estoque():
+    wb = load_workbook('Estoque.xlsx')
+    ws = wb["SmartHaus"]
+    while True:
+        nome = input("Nome do produto:")
+        if len(nome) != 0:
+            break
+        else:
+            print("Campo obrigatório")
+    while True:
+        qnt = input("Quantos estão entrando no estoque?")
+        if qnt.isdigit():
+            break
+        elif len(qnt) == 0:
+            qnt = 0
+            break
+        else:
+            print("Quantidade deve ser números")
+    while True:
+        valor = input("Valor por unidade:")
+        if valor.isdigit():
+            valor = int(valor)
+            if valor > 0:
+                break
+            else:
+                print("O valor não pode ser 0")
+        else:
+            print("O valor deve ser número")
+    while True:
+        descricao = input("Descrição do produto:")
+        if len(descricao) == 0:
+            descricao = "Produto sem descrição."
+            break
+        else:
+            break
+    while True:
+        marca = input("Marca do produto:")
+        if len(marca) != 0:
+            break
+        else:
+            print("Campo obrigatório")
+    end_row = len(ws["A"]) + 1
+    ws["A" + str(end_row)] = nome
+    ws["B" + str(end_row)] = qnt
+    ws["C" + str(end_row)] = valor
+    ws["D" + str(end_row)] = descricao
+    ws["E" + str(end_row)] = marca
+    estoque.append(Produtos(nome,qnt,valor,descricao,marca))
+    print("Produto adicionado ao estoque!")
+    wb.save('Estoque.xlsx')
+    
+wb.save('Estoque.xlsx')
+
+adicionar_estoque()
