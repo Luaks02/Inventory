5ebec09994a4289ce6fedd6058f974f5f1b44e3d
Lucas Martins <Lucascamilo02@gmail.com>
2022-12-03 20:35:30 -0300

    Started to adapt interface from project CRUD

--- Inventory.py
+++ Inventory.py
@@ -1,6 +1,10 @@
 from openpyxl import workbook, load_workbook
 from openpyxl.utils import get_column_letter
 import xlwings
+import pandas
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
 #Abrindo a planilha e iniciando variavel de quantidade de linhas já usadas no Estoque
 wb = load_workbook('Estoque.xlsx')
@@ -16,12 +20,6 @@ class Produtos:
         self.valor = valor
         self.descricao = descricao
         self.marca = marca
-
-#Subindo as informações da planilha banco de dados para o código
-estoque = []
-
-for row in range(2,end_row+1):
-    estoque.append(Produtos(ws["A"+str(row)].value,ws["B"+str(row)].value,ws["C"+str(row)].value,ws["D"+str(row)].value,ws["E"+str(row)].value,))
 
 #Adicionando novos produtos ao banco de dados
 def adicionar_estoque():
@@ -71,7 +69,6 @@ def adicionar_estoque():
     ws["C" + str(end_row)] = valor
     ws["D" + str(end_row)] = descricao
     ws["E" + str(end_row)] = marca
-    estoque.append(Produtos(nome,qnt,valor,descricao,marca))
     print("Produto adicionado ao estoque!")
     wb.save('Estoque.xlsx')
     
@@ -140,4 +137,50 @@ def reserved_list(cliente):
             return print("Estes são os itens reservados") 
     print("Falha")
 
-reserved_list("teste")
+#Iniciando a janela de interface
+
+janela = Tk()
+janela.title("Gerenciamento Galpão")
+janela.geometry("670x370")
+janela.configure(bg="#ececec")
+
+ttk.Separator(janela, orient=HORIZONTAL).grid(row=0,columnspan=1,ipadx=272)
+
+style = ttk.Style(janela)
+style.theme_use("clam")
+
+janela.after(1, lambda: janela.focus_force())
+
+#Inserindo os elementos para interação
+
+tree = ttk.Treeview(janela, columns=("0","1","2","3"),show="headings")
+tree.heading("0", text="ID")
+tree.column("0",anchor="center", stretch="no", width=30)
+tree.heading("1", text="Nome do Produto")
+tree.column("1",anchor="center", stretch="no", width=200)
+tree.heading("2", text="Qnt")
+tree.column("2",anchor="center", stretch="no", width=50)
+tree.heading("3", text="Descrição")
+tree.column("3", stretch="no", width=320)
+tree.place(x=30,y=100)
+
+entrada = Entry(janela, width=100)
+entrada.focus_set()
+entrada.place(x=30, y=70)
+
+b_create = Button(janela, text="Novo Produto", relief="raised", overrelief=RIDGE, anchor=NW, font=("Verdana 12"), bg="#D3D4D3", fg="#080808")
+b_create.place(x=30,y=20)
+
+b_alt_desc = Button(janela, text="Alterar Descrição", relief="raised", overrelief=RIDGE, anchor=NW, font=("Verdana 12"), bg="#D3D4D3", fg="#080808")
+b_alt_desc.place(x=170,y=20)
+
+b_update = Button(janela, text="Entrada/Saída", relief="raised", overrelief=RIDGE, anchor=NW, font=("Verdana 12"), bg="#D3D4D3", fg="#080808")
+b_update.place(x=340,y=20)
+
+b_delete = Button(janela, text="Deletar Produto", relief="raised", overrelief=RIDGE, anchor=NW, font=("Verdana 12"), bg="#D3D4D3", fg="#080808")
+b_delete.place(x=490,y=20)
+
+b_importar = Button(janela, text="Importar Dados CSV", relief="raised", overrelief=RIDGE, anchor=NW, font=("Verdana 12"), bg="#D3D4D3", fg="#080808")
+b_importar.place(x=475,y=332)
+
+janela.mainloop()
